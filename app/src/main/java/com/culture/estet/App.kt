b4f8d2a3bcbc8f17aaa02d524bf6e0a239dedd7d
Andrew <labworkspace@yandex.ru>
2023-05-22 23:44:35 +0300

    Add map

--- app/src/main/java/com/culture/estet/App.kt
+++ app/src/main/java/com/culture/estet/App.kt
@@ -1,8 +1,13 @@
 package com.culture.estet
 
 import android.app.Application
+import com.yandex.mapkit.MapKitFactory
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        MapKitFactory.setApiKey(getString(R.string.API_KEY))
+    }
 }
